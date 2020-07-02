6c6a545b9130030f7d55f3ec5af6b2674e77cca4
张磊磊 <@DESKTOP-4C8K0UK>
2020-07-02 09:25:36 +0800

    添加挂号相关service服务

---
+++ hospital/src/main/java/com/five/service/RegeditService.java
@@ -0,0 +1,16 @@
+package com.five.service;
+
+import java.util.Map;
+
+/**
+ * 	挂号相关服务
+ * @author 张磊磊
+ *
+ */
+public interface RegeditService {
+
+	//查询当前时间
+	Map<String,String> getDate();
+	
+	
+}
---
+++ hospital/src/main/java/com/five/service/impl/RegeditSerivceImpl.java
@@ -0,0 +1,15 @@
+package com.five.service.impl;
+
+import java.util.Map;
+
+import com.five.service.RegeditService;
+
+public class RegeditSerivceImpl implements RegeditService{
+
+	@Override
+	public Map<String, String> getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
